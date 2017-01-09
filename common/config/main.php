47a21e8ab53cbbd286c50f68120024a1de9bae2c
liemnh267@gmail.com <liemnh267@gmail.com>
2017-01-09 22:36:52 +0700

    liemnh

--- common/config/main.php
+++ common/config/main.php
@@ -8,8 +8,8 @@ return [
         ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=noithat_minhdoan',
-            'username' => 'noithat_minhdoan',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=thietkenoithat_db',
+            'username' => 'thietkenoithat',
             'password' => '123456',
             'charset' => 'utf8',
         ],
